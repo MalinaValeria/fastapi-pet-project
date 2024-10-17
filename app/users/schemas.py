3bd47b1ca993845f1c8161ea1dfcd75e39171b5a
Valeria Malina <wollt1362@gmail.com>
2024-10-17 17:58:12 +0300

    Add models

---
+++ app/users/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class SUserRegister(BaseModel):
+    email: EmailStr = Field(..., description='Email address')
+    password: str = Field(..., min_length=8, max_length=50, description='Password')
+    password_confirm: str = Field(..., min_length=8, max_length=50, description='Confirm Password')
+    name: str = Field(..., min_length=2, max_length=50, description='Name')
+
+
+class SUserAuth(BaseModel):
+    email: EmailStr = Field(..., description='Email address')
+    password: str = Field(..., min_length=8, max_length=50, description='Password')
